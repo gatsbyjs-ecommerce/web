25a4dc0ab107c89efac50d39e7b37651fada9f7d
Parminder Klair <perminder.klair@gmail.com>
2020-01-18 15:35:48 +0530

    script fix

--- src/html.js
+++ src/html.js
@@ -31,6 +31,13 @@ export default class HTML extends React.Component {
             rel="stylesheet"
           />
           <script src="https://checkout.razorpay.com/v1/checkout.js" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/cc89738f5e13735361f5b9aa9/fe7752cb372831b7a2090efd4.js");
+              `,
+            }}
+          />
           {/* Add custom css or scripts here */}
 
           {this.props.headComponents}
