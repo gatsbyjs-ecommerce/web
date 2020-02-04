5b33c91eb1ecb3fb045c1d78f8cb5ee2091c0903
Parminder Klair <perminder.klair@gmail.com>
2020-02-04 22:26:33 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   debug: process.env.NODE_ENV === 'development',
 
-  siteName: 'gatsby-ecommerce',
+  siteName: '--siteName--',
   author: 'Parminder Klair',
   description: 'Premium Products Shop',
   logo: '/images/logo.png',
-  siteUrl: 'https://www.site.com',
+  siteUrl: '--siteUrl--',
   graphQlUri: 'https://site-api.now.sh/api',
   graphQlUriDev: 'http://localhost:4000/api',
   projectKey: '123123abc',
@@ -14,7 +14,7 @@ module.exports = {
   googleAnalytics: '',
   backgroundColor: '#e0e0e0',
   themeColor: '#E38D60',
-  sanityId: '2jkk6tlv',
+  sanityId: '--sanityId--',
   razorPayKey: '',
   mailchimp: '',
 };
